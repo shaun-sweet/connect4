7f7f7ba6fa52774c83ac5d3167569b9d49df342f
sweetkeys <shaun@shaunsweet.com>
2016-07-18 20:15:33 -0700

    last part of algorithm being worked on

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,29 @@
-class ConnectFour {
+// Possible directions:  Horizontal, vertical, left diagnal \ and right diagnal /
+const winConditions = {
 
+  horizontal: {
+    rightHorizontal: {row: 0, column: 1},
+    leftHorizontal: {row: 0, column: -1}
+  },
+
+  vertical: {
+    downVertical: {row: 1, column: 0},
+    upVertical: {row: -1, column: 0}
+  },
+
+  leftDiagonal: {
+    downLeftDiagonal: {row: 1, column: 1},
+    upLeftDiagonal: {row: -1, column: -1}
+  },
+
+  rightDiagonal: {
+    upRightDiagonal: {row: -1, column: 1},
+    downRightDiagonal: {row: 1, column: -1}
+  }
+}
+
+class ConnectFour {
+  
   constructor(){
     this.player1;
     this.player2;
@@ -7,36 +31,82 @@ class ConnectFour {
     this._populateBoard();
   }
 
+
   log() {
     return this.board;
   }
 
-  test() {
-    return this.board[6];
-  }
-
   playTurn(column, player) {
-    this._checkIfColumnFull(column);
+    if (this._ifColumnFull(column)) {
+      return "This isn't a valid move";
+    }
     for (var row = 0; row < 6; row ++) {
-      // Finds the first "placed piece" and places one, one row above it
-      if (this.board[row][column] == null && row == 5) {
-        return this.board[row][column] = player;
-      }
       // if column hasn't been played, place piece on the bottom
+      if (this.board[row][column] == null && row == 5) {
+        this.board[row][column] = player;
+        // console.log(this._checkForWin(row, column));
+        break;
+      }
+      // Finds the first "placed piece" and places one, one row above it
       if (this.board[row][column] != null && row != 0) {
-        return this.board[row - 1][column] = player;
+         this.board[row - 1][column] = player;
+         // console.log(this._checkForWin(row, column));
+         break;
       }
     }
   }
 
+  _checkForWin(row, column) {
+    var color = this.board[row][column];
+    for (var winCondition in winConditions) {
+      var successiveColors = 1;
+      for (var directions in winConditions[winCondition]) {
+        this._checkAdjacentSpaces();
+        // var nextCell = {
+        //   row: row + winConditions[winCondition][directions].row,
+        //   column: column + winConditions[winCondition][directions].column
+        // };
+        // // Make sure the check is within bounds of the board
+        // if (this._cellOutOfBounds(nextCell)) {
+        //   continue;
+        // }else{
+        //   if (this.board[nextCell.row][nextCell.column] === color) {
+        //     successiveColors ++;
+        //   }else{
+        //     continue;
+        //   }
+        // }
+      }
+      if (successiveColors > 3) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-  checkForWin() {
-    // diagnals are only possible on column 3 (0 indexed)
-    // column 3 is the only column that needs both right horizontal and left checked
-    // column 4-6 only need horizontal checked to the left of the piece
-    // column 0-2 only need horizontal checked to the right of the piece
-    // row 0-2 only need vertical checked downwards
-    // row 3-5 only need vertical checked upwards
+  _checkAdjacentSpaces() {
+    var nextCell = {
+      row: row + winConditions[winCondition][directions].row,
+      column: column + winConditions[winCondition][directions].column
+    };
+    // Make sure the check is within bounds of the board
+    if (this._cellOutOfBounds(nextCell)) {
+      continue;
+    }else{
+      if (this.board[nextCell.row][nextCell.column] === color) {
+        successiveColors ++;
+      }else{
+        continue;
+      }
+    }
+  }
+
+  _cellOutOfBounds(nextCell) {
+    if (nextCell.row < 0 || nextCell.row > 5 || nextCell.column < 0 || nextCell.column > 6) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   _populateBoard() {
@@ -49,21 +119,25 @@ class ConnectFour {
     }
   }
 
-  _checkIfColumnFull(column) {
+  _ifColumnFull(column) {
     if (this.board[0][column] != null) {      
-      return "Invalid Move";
+      return true;
     }
+    return false;
   }
 
 }
 
 var a = new ConnectFour();
 a.playTurn(0, 'black');
-a.playTurn(0, 'red');
-a.playTurn(1, 'red');
-a.playTurn(2, 'black');
-a.playTurn(0, 'red');
-a.playTurn(0, 'red');
-a.playTurn(0, 'red');
+a.playTurn(0, 'black');
+a.playTurn(0, 'black');
+a.playTurn(0, 'black');
+
+// a.playTurn(0, 'red');
+// a.playTurn(0, 'red');
+// a.playTurn(0, 'red');
+// a.playTurn(0, 'red');
+// a.playTurn(0, 'black');
 console.log(a.log());
-console.log(a.test());
+// console.log(a.test());
